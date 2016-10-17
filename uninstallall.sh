e7baa84a333e3c89ce3a92921c696de695458b70
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2016-10-17 21:36:34 +0200

    Fix uninstall script on Mac

--- uninstallall.sh
+++ uninstallall.sh
@@ -8,4 +8,4 @@ fi
 
 
 args=("$@")
-adb shell pm list packages | grep "${args[0]}" | cut -d ':' -f2 | tr '\r' ' ' | xargs -t -r -n1   adb uninstall
+adb shell pm list packages | grep "${args[0]}" | cut -d ':' -f2 | tr '\r' ' ' | xargs -t -n1 adb uninstall
