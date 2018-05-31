db1980a7de363ecf8fc939123c55f83fb8650d51
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2018-05-31 14:42:17 +0200

    Add clearall script

---
+++ clearall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+
+args=("$@")
+adb shell pm list packages | grep "${args[0]}" | cut -d ':' -f2 | tr '\r' ' ' | xargs -t -n1 adb shell pm clear
